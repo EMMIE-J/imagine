f702db4c8567b339e8f1cb44f8f1b5561b49cf12
emmie-j <attachiegrace408@gmail.com>
2024-06-06 12:14:02 +0000

    math in javascript

--- public/js/script.js
+++ public/js/script.js
@@ -36,6 +36,38 @@ console.log(person)
 person.username = 'Mimi'
 console.log(person.username);
 
+  // Functions
+//  Defining a function
+function login(username, password) {
+    // Validate username and password
+    if (!username || !password) {
+      return 'Username or password not provided';
+    }
+    if (username =='emmie' && password == '1234') {
+      return 'User is logged in';
+    } else {
+      return 'Invalid username or password';
+    }
+  }
   
+  // Invoking a function
+  login('emmie');
+  
+  
+  //  Basic Arithmetic Operations
+  11+12;
+  4+6;
+  5-3;
+  50-35;
+  60/7;
+  5*6;
+  14 % 3;
+  
+  Math.floor(45/24);
+  45 % 24
+  Math.random() * 1000
+  Math.max(34,32,37);
+  2* (3+4) - 5/2;
+  Number('ty') + 2
   
 
